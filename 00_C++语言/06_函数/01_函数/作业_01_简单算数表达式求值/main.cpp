1ed7870bef0d3546a255266791eacca5b21fff57
Future. 魏来 <624263934@qq.com>
2024-12-04 23:42:09 +0800

    更新：添加 作业 简单算数表达式求值 参考实例

--- 00_C++语言/06_函数/01_函数/作业_01_简单算数表达式求值/main.cpp
+++ 00_C++语言/06_函数/01_函数/作业_01_简单算数表达式求值/main.cpp
@@ -26,10 +26,19 @@ int js(int x, char z, int y) {
 }
 
 int main() {
-
 	int a, b;
 	char c;
+
+	// --------------------------------
+	// 
+	// 如果使用 visual studio 报错
+	// 在 项目>属性>C/C++>SDL检查 改为 否
+	// 
+	//---------------------------------
+	
 	scanf("%d%c%d", &a, &c, &b);
+	
 	printf("%d", js(a, c, b));
+	
 	return 0;
 }
