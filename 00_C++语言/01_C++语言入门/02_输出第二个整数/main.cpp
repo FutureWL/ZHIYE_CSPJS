ea0158b0e29b2d1385eadf0365fe9c74cef457a0
FutureWL <624263934@qq.com>
2024-12-22 03:37:49 +0800

    新增：C++ 语言入门

---
+++ 00_C++语言/01_C++语言入门/02_输出第二个整数/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    // 提示用户输入两个整数
+    cout << "请输入两个整数，用空格隔开：";
+    // 接收用户输入的两个整数
+    cin >> num1 >> num2;
+    // 输出第二个整数
+    cout << "第二个整数是： " << num2 << endl;
+
+    return 0;
+}
