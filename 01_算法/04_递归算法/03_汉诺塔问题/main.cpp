e911048423e55f99940729f663899b1c34f8c3d7
FutureWL <624263934@qq.com>
2025-01-07 18:40:39 +0800

    更新：优化内容

--- 01_算法/04_递归算法/03_汉诺塔问题/main.cpp
+++ 01_算法/04_递归算法/03_汉诺塔问题/main.cpp
@@ -1,19 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
+
+// 设置文件编码为 UTF-8
+#pragma execution_character_set("utf-8")
 
 // 汉诺塔移动步骤的递归函数
 void hanoi(int n, char from, char aux, char to) {
     if (n == 1) {
-        std::cout << "将第1个圆盘从 " << from << " 柱移到 " << to << " 柱" << endl;
+        cout << "将第1个圆盘从 " << from << " 柱移到 " << to << " 柱" << endl;
         return;
     }
     hanoi(n - 1, from, to, aux);
-    std::cout << "将第" << n << "个圆盘从 " << from << " 柱移到 " << to << " 柱" << endl;
+    cout << "将第" << n << "个圆盘从 " << from << " 柱移到 " << to << " 柱" << endl;
     hanoi(n - 1, aux, from, to);
 }
 
 int main() {
+
     int numDisks;
     cout << "请输入汉诺塔圆盘的个数: ";
     cin >> numDisks;
